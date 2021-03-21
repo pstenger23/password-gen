04b69f98025572d89e74a68530f79652717753f9
P Stenger <pstenger23@gmail.com>
2021-03-21 11:52:05 -0500

    for loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,18 @@ var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*?abcdefghijklmnopqrstuvwxyz12345
 var lowerSpecial = "abcdefghijklmnopqrstuvwxyz!@#$%&*?1234567890"
 var capSpecial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*?1234567890"
 var capOnly = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowerOnly = "abcdefghijklmnopqrstuvwxyz"
+var noSpecial = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 generatePassword = function() {
   var generated="";
   var numberOf = prompt("How many characters would you like your password to be? choose a number between 8 and 128.");
-  var upperLower = confirm("Would you like upper and lowercase letters? Choose 'OKAY' for both, and 'CANCEL' for uppercase only.");
-  var specialConfirm = confirm("Choose OKAY if you would like special characters and CANCEL if you do not.");
+  
+  
   
   if(numberOf>=8 && numberOf<=128){
+
+    var upperLower = confirm("Would you like upper and lowercase letters? Choose 'OKAY' for both, and 'CANCEL' for uppercase only.");
+    var specialConfirm = confirm("Choose OKAY if you would like special characters and CANCEL if you do not.");
 
     if(upperLower && specialConfirm){
       for(var i=0; i<numberOf; i++){
@@ -22,12 +25,31 @@ generatePassword = function() {
       }
       return generated;
     }
-    else {
-      alert("You must choose a valid optin, try again.");
-      generatePassword();
+    if(!upperLower && specialConfirm){
+      for(var i=0; i<numberOf; i++){
+        generated += capSpecial.charAt(Math.floor(Math.random() * numberOf))
+        console.log(generated);
+      }
+      return generated;
+    }
+    if(upperLower && !specialConfirm){
+      for(var i=0; i<numberOf; i++){
+        generated += capSpecial.charAt(Math.floor(Math.random() * numberOf))
+        console.log(generated);
+      }
+    }
+    if(!upperLower && !specialConfirm){
+      for(var i=0; i<numberOf; i++){
+        generated += capOnly.charAt(Math.floor(Math.random() * numberOf))
+        console.log(generated);
+      }
+      return generated;
     }
   }
-
+  else {
+    alert("you must choose a valid number of characters.")
+    generatePassword();
+  }
 }
 
 // Write password to the #password input
