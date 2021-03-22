b58af51d844e4c1ec19940afd8aa943130e48538
P Stenger <pstenger23@gmail.com>
2021-03-21 21:24:37 -0500

    more password options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var capOnly = "AFQDHXOVJLETKCGPUIRMNYWBSZ"
-var lowerOnly = "avfgpxscbutimnyqhrkjlowdez"
-var lettersOnly = "abmQlFitcJkSLnzxyrdgHMGoPqAUjDesCwVXOvRKIupBEfThWNYZ"
-
-var specialOnly = "?*&%$#@!.6253814790?*&%$#@!."
-var lowerSpecial = "abcdefghijklmnopqrstuvwxyz!@#$%&*?."
-var capSpecial = "AFBGECDH5ILKMJN!OPQR$SUTV&WXYZ@#$.%&*?"
-
 var fullCharset = "ACFGEDBH5ILJMKN!OPQR$STUV&WXYZ%*?adbce78fhijgk@lm#nop2q9rsuvtxwyz13460"
+var lowerSpecial = "abcdefghijklmnopqrstuvwxyz!@#$%&*?."
+var specialOnly = "?*&%$#@!.6253814790?*&%$#@!."
+var lettersOnly = "abmQlFitcJkSLnzxyrdgHMGoPqAUjDesCwVXOvRKIupBEfThWNYZ"
+var upperOnly = "AFQDHXOVJLETKCGPUIRMNYWBSZ"
+var lowerOnly = "avfgpxscbutimnyqhrkjlowdez"
+var capSpecial = "AFBGECDH5ILKMJNOPQR$SUTV&WXYZ@#$!.%&*?"
+
+
+
 generatePassword = function() {
   //where the generated password goes
   var generated="";
@@ -17,7 +18,7 @@ generatePassword = function() {
   var numberOf = prompt("How many characters would you like your password to be? choose a number between 8 and 128.");
   var upperConfirm = confirm("Would you like capital letters? Choose OKAY if Yes, and 'CANCEL' if No.");
   var lowerConfirm = confirm("Would you like lowercase letters? Choose 'OKAY' if Yes, and 'CANCEL' if No.");
-  var specialConfirm = confirm("Would you like SPecial Characters and Numbers in your password? Choose 'OKAY' if Yes, and CANCEL if No.");
+  var specialConfirm = confirm("Would you like Secial Characters and Numbers in your password? Choose 'OKAY' if Yes, and CANCEL if No.");
   
   
   //if number of characters is correct
@@ -25,6 +26,7 @@ generatePassword = function() {
 
     //IF ALL CHARACTERS
     case (upperConfirm && lowerConfirm && specialConfirm):
+      
       for(var i=0; i<numberOf; i++){
         generated += fullCharset.charAt(Math.floor(Math.random() * numberOf));
       }
@@ -37,11 +39,25 @@ generatePassword = function() {
         console.log(generated);
       }
       return generated;
+
+    case(!upperConfirm && !lowerConfirm && specialConfirm):
+      for(var i=0; i<numberOf; i++){
+        generated += specialOnly.charAt(Math.floor(Math.random() * numberOf))
+        console.log(generated);
+      }
+      return generated;
     
     //IF BOTH UPPER AND LOWER CASE ONLY
     case (upperConfirm && lowerConfirm && !specialConfirm):
       for(var i=0; i<numberOf; i++){
-        generated += noSpecial.charAt(Math.floor(Math.random() * numberOf))
+        generated += lettersOnly.charAt(Math.floor(Math.random() * numberOf))
+        console.log(generated);
+      }
+      return generated;
+    
+    case(upperConfirm && !lowerConfirm && !specialConfirm):
+      for(var i=0; i<numberOf; i++){
+        generated += upperOnly.charAt(Math.floor(Math.random() * numberOf))
         console.log(generated);
       }
       return generated;
@@ -49,12 +65,18 @@ generatePassword = function() {
     //IF UPPER CASE ONLY
     case (!upperConfirm && !specialConfirm && lowerConfirm):
       for(var i=0; i<numberOf; i++){
-        generated += capOnly.charAt(Math.floor(Math.random() * numberOf))
+        generated += lowerOnly.charAt(Math.floor(Math.random() * numberOf))
         console.log(generated);
       }
       return generated;
     
-  }
+    case (upperConfirm && specialConfirm && !lowerConfirm):
+      for(var i=0; i<numberOf; i++){
+        generated += capSpecial[(Math.floor(Math.random() * numberOf))]
+        console.log(generated);
+      }
+      return generated;
+}
   
 }
 
